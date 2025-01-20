21c20f07f21490e7fa8ec89a13e77c90c21b90fb
Fabian Oleksiuk <fabbbian@interia.pl>
2025-01-20 11:06:29 +0100

    C

--- Grafik/Program.cs
+++ Grafik/Program.cs
@@ -52,6 +52,23 @@ builder.Services.AddTransient<ICustomAuthService, CustomAuthService>();
 
 var app = builder.Build();
 
+// Apply migrations on startup
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    try
+    {
+        var context = services.GetRequiredService<ApplicationDbContext>();
+        context.Database.Migrate();
+    }
+    catch (Exception ex)
+    {
+        var logger = services.GetRequiredService<ILogger<Program>>();
+        logger.LogError(ex, "An error occurred while migrating the database.");
+    }
+}
+
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
